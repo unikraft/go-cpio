e638a1b0d3dccadcd6feb7b92bdf7e89d616c465
Justin Chadwell <justin@unikraft.com>
2026-02-09 10:55:04 +0000

    chore: Add SPDX license headers
    Signed-off-by: Justin Chadwell <justin@unikraft.com>

--- writer_test.go
+++ writer_test.go
@@ -1,3 +1,8 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2017, Ryan Armstrong.
+// Licensed under the BSD-3-Clause License (the "License").
+// You may not use this file except in compliance with the License.
+
 package cpio_test
 
 import (
